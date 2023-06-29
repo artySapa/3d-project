d90c7613da66d203f213ded1a741492a6a45d6b2
Artyom Sapa <artem.sapa@gmail.com>
2023-06-28 22:28:48 -0700

    added input UI for posts

--- src/components/MainGrid.jsx
+++ src/components/MainGrid.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import axios from "axios";
 
 const MainGrid = () => {
+    const URL = "http://localhost:8080";
+
+    const [entries, setEntries] = useState([]);
+
+    /* FOR ADDITIONS */
+    const [entryContents, setEntryContents] = useState("");
+    const [rank, setRank] = useState(-5);
+    /* ------------- */
+
+    const addPost = () => {
+        axios
+        .post(`${URL}/entries/new`, {
+            content: entryContents,
+            rank: rank,
+            timestamp: Date.now(),
+        })
+        .then((response) => {
+            setEntries((prevEntries) => {
+                return [...prevEntries, response.data];
+            });
+        })
+        .catch(console.error);
+
+        setRank(-5);
+        setEntryContents("");
+    }
+
+    const getFeed = () => {
+       axios
+       .get(URL + "/entries")
+       .then((response) => {
+        setEntries(response.data);
+       })
+       .catch(console.error);
+    };
+
+    useEffect(() => {
+        getFeed();
+    }, [entries]);
+
     return (
-        <div>
-            MainGrid
+        <div className="p-20 flex justify-between">
+            <input className="p-5" type="text" value={entryContents} onChange={(e) => {setEntryContents(e.target.value)}} placeholder="Give a name" />
+            <input className="p-5" type="text" value={rank} onChange={(e) => {setRank(e.target.value)}} placeholder="Set the rank" />
+            <button className="bg-[pink] p-2 text-black" onClick={() => {addPost();}}>ADD REQUEST TO THE COMMUNITY</button>
+            <div>
+
+            </div>
         </div>
     );
 }
