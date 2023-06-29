90295f627709a59656e5d6f6ebbcc02f5268b832
Artyom Sapa <artem.sapa@gmail.com>
2023-06-29 14:59:17 -0700

    background

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,12 +5,12 @@ import IntroCanvas from "./canvas/IntroCanvas";
 const Welcome = () => {
     return (
         <section className={`relative flex w-full h-screen mx-auto justify-center align-center`}>
-            <div className="p-40 pl-12">
+            <div className="p-40 pl-12 text-black">
                 <div className="flex justify-between mb-10">
-                <p className="text-5xl font-bold">Hello, my name is <span className="text-[lightgreen]">Arty</span></p>
+                <p className="text-5xl font-bold">Hello, my name is <span className="text-[#50C878]">Arty</span></p>
                 </div>
-                <p className="mt-30 text-2xl">This project is for you to play around with 3d objects and for me to learn more about the ThreeJS library :)</p>
-                <div className="flex h-[600px]">
+                <p className="mt-30 text-2xl max-w-[490px]">This project is for you to play around with 3d objects and for me to learn more about the ThreeJS library :)</p>
+                <div className="h-[600px]">
                 <IntroCanvas />
                 </div>
             </div>
