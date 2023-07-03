638032e8d035a1253f50e6f4be7bb01a8972ddae
Artyom Sapa <artem.sapa@gmail.com>
2023-07-03 14:54:02 -0700

    frontend for login page

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,9 +1,51 @@
 import React from "react";
 
 const Login = () => {
-    return (
-        <div className="mt-[100px] ">Goodbie</div>
-    );
-}
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-primary">
+      <div className="max-w-md w-full px-6 py-8 bg-white shadow-md rounded-md">
+        <h2 className="text-2xl font-bold mb-8 text-black text-center">Log In</h2>
+        <form className="space-y-6">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="mt-1 p-2 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md h-10"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="mt-1 p-2 h-10 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
+              Log In
+            </button>
+          </div>
+          <div className="flex justify-center">
+          <p className="text-black text-xs">Don't have an account?</p>
+          <button className="text-[blue] text-xs ml-5">Sign in</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
