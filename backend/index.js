fb1edb2e9adfefd50ddf7e82f5e401efcbecf557
Artyom Sapa <artem.sapa@gmail.com>
2024-10-14 17:53:15 -0700

    removed react-redux import in backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 require('dotenv').config(); // Load environment variables from .env
-const { useSelector } = require("react-redux");
 
 const mongoose = require("mongoose");
 const cors = require("cors");
